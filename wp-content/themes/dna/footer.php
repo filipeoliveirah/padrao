d07723c34cddcf0b2035c6b9307421f51c142729
Filipe Oliveira <filipeoliveirah@gmail.com>
2018-05-02 18:31:54 -0300

    Homologacao

--- wp-content/themes/dna/footer.php
+++ wp-content/themes/dna/footer.php
@@ -1,55 +1,57 @@
 <footer>
-    <div class="container footer-atendimento">
-        <div class="col-md-2">
-            <h3>Laboratório Padrão</h3>    
-            <ul>
-                <p>O Laboratório Padrão tem como missão assegurar confiança, precisão e rapidez no auxílio diagnóstico e no cuidado com a saúde. Oferece exames de Análises Clínicas, Diagnóstico por Imagem e também Vacinação em todo estado de Goiás.</p>
-            </ul>
-        </div>
-        <div class="col-md-2">
-            <h3>Institucional</h3>
-            <ul class="Internas">
-                <li><a href="http://www.padrao.com.br/br/p/80/a-empresa----capa.aspx" title="A Empresa">A Empresa</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/58/servicos---capa.aspx" title="Serviços">Serviços</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/60/unidades---capa.aspx" title="Unidades">Unidades</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/177/assessoria-cientifica.aspx" title="Assessoria Científica">Assessoria Científica</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/144/sala-de-imprensa.aspx" title="Sala de Imprensa">Sala de Imprensa</a></li>
-                <li><a href="http://www.vagas.com/grupohermespardini" title="Trabalhe Conosco">Trabalhe Conosco</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/75/fala-conosco---capa.aspx" title="Fale Conosco">Fale Conosco</a></li>
-                <li><a href="http://webmail.padrao.com.br" title="Webmail">Webmail</a></li>
-            </ul>
-        </div>
-        <div class="col-md-2">
-            <h3>Análises Clínicas</h3>
-            <ul class="Internas">
-                <li><a href="http://www.padrao.com.br/br/p/83/atendimento-domiciliar---capa.aspx" title="Atendimento Domiciliar">Atendimento Domiciliar</a></li>
-                <li><a href="http://weblis.padrao.com.br/cache/csp/padrao/MV.FC.cls?w3Exec=WLR010" title="Exames e Preparos">Exames e Preparos</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/87/planos-de-saude----capa.aspx" title="Planos de Saúde">Planos de Saúde</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/60/unidades---capa.aspx" title="Unidades">Unidades</a></li>
-            </ul>
-        </div>
-        <div class="col-md-2">
-            <h3>Vacinas</h3>
-            <ul class="Internas">
-                <li><a href="http://www.padrao.com.br/br/p/83/atendimento-domiciliar---capa.aspx" title="Atendimento Domiciliar">Vacinas</a></li>
-                <li><a href="http://weblis.padrao.com.br/cache/csp/padrao/MV.FC.cls?w3Exec=WLR010" title="Exames e Preparos">Saúde em dia</a></li>
-            </ul>
-        </div>
-        <div class="col-md-2">
-            <h3>Profissionais da Saúde</h3>
-            <ul class="Internas"><li><a href="http://www.padrao.com.br/br/p/135/algoritmos.aspx" title="Algoritmos">Algoritmos</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/177/assessoria-cientifica.aspx" title="Assessoria Científica">Assessoria Científica</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/137/boletins-medicos.aspx" title="Boletins técnicos">Boletins técnicos</a></li>
-            </ul>
-        </div>
-        <div class="col-md-2">
-            <h3>Diagnóstico por Imagem</h3>
-            <ul class="Internas"><li><a href="http://www.padrao.com.br/br/p/69/agendamento---capa.aspx" title="Pré-agendamentos">Pré-agendamentos</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/70/corpo-clinico---capa.aspx" title="Corpo Clínico">Corpo Clínico</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/66/exames---capa.aspx" title="Exames">Exames</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/72/planos-de-saude----capa.aspx" title="Planos de Saúde">Planos de Saúde</a></li>
-                <li><a href="http://www.padrao.com.br/br/p/60/unidades---capa.aspx" title="Unidades">Unidades</a></li>
-            </ul>
+    <div style="background-color: #eee">
+        <div class="container footer-atendimento">
+            <div class="col-md-2">
+                <h3>Laboratório Padrão</h3>    
+                <ul>
+                    O Laboratório Padrão tem como missão assegurar confiança, precisão e rapidez no auxílio diagnóstico e no cuidado com a saúde. Oferece exames de Análises Clínicas, Diagnóstico por Imagem e também Vacinação em todo estado de Goiás.
+                </ul>
+            </div>
+            <div class="col-md-2 divisoria-lateral">
+                <h3>Institucional</h3>
+                <ul class="Internas">
+                    <li><a href="http://www.padrao.com.br/br/p/80/a-empresa----capa.aspx" title="A Empresa">A Empresa</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/58/servicos---capa.aspx" title="Serviços">Serviços</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/60/unidades---capa.aspx" title="Unidades">Unidades</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/177/assessoria-cientifica.aspx" title="Assessoria Científica">Assessoria Científica</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/144/sala-de-imprensa.aspx" title="Sala de Imprensa">Sala de Imprensa</a></li>
+                    <li><a href="http://www.vagas.com/grupohermespardini" title="Trabalhe Conosco">Trabalhe Conosco</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/75/fala-conosco---capa.aspx" title="Fale Conosco">Fale Conosco</a></li>
+                    <li><a href="http://webmail.padrao.com.br" title="Webmail">Webmail</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 divisoria-lateral">
+                <h3>Análises Clínicas</h3>
+                <ul class="Internas">
+                    <li><a href="http://www.padrao.com.br/br/p/83/atendimento-domiciliar---capa.aspx" title="Atendimento Domiciliar">Atendimento Domiciliar</a></li>
+                    <li><a href="http://weblis.padrao.com.br/cache/csp/padrao/MV.FC.cls?w3Exec=WLR010" title="Exames e Preparos">Exames e Preparos</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/87/planos-de-saude----capa.aspx" title="Planos de Saúde">Planos de Saúde</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/60/unidades---capa.aspx" title="Unidades">Unidades</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 divisoria-lateral">
+                <h3>Vacinas</h3>
+                <ul class="Internas">
+                    <li><a href="http://www.padrao.com.br/br/p/83/atendimento-domiciliar---capa.aspx" title="Atendimento Domiciliar">Vacinas</a></li>
+                    <li><a href="http://weblis.padrao.com.br/cache/csp/padrao/MV.FC.cls?w3Exec=WLR010" title="Exames e Preparos">Saúde em dia</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 divisoria-lateral">
+                <h3>Profissionais da Saúde</h3>
+                <ul class="Internas"><li><a href="http://www.padrao.com.br/br/p/135/algoritmos.aspx" title="Algoritmos">Algoritmos</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/177/assessoria-cientifica.aspx" title="Assessoria Científica">Assessoria Científica</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/137/boletins-medicos.aspx" title="Boletins técnicos">Boletins técnicos</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 divisoria-lateral">
+                <h3>Diagnóstico por Imagem</h3>
+                <ul class="Internas"><li><a href="http://www.padrao.com.br/br/p/69/agendamento---capa.aspx" title="Pré-agendamentos">Pré-agendamentos</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/70/corpo-clinico---capa.aspx" title="Corpo Clínico">Corpo Clínico</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/66/exames---capa.aspx" title="Exames">Exames</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/72/planos-de-saude----capa.aspx" title="Planos de Saúde">Planos de Saúde</a></li>
+                    <li><a href="http://www.padrao.com.br/br/p/60/unidades---capa.aspx" title="Unidades">Unidades</a></li>
+                </ul>
+            </div>
         </div>
     </div>
    <div class="footer-atendimento">
